010f45168447c1a46724a203cc158f0765d75b44
Khaeruddin Asdar <46445417+Khaeruddinasdar12@users.noreply.github.com>
2021-08-16 22:39:01 +0800

    perbaiki bentuk api setting

--- app/Http/Controllers/Api/SettingController.php
+++ app/Http/Controllers/Api/SettingController.php
@@ -13,28 +13,44 @@ class SettingController extends Controller
     public function kategoriPerusahaan()
     {
         $data = KategoriPerusahaan::select('id', 'nama')->get();
-
-        return $data;
+        
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Semua Kategori Perusahaan',
+            'data' => $data
+        ]); 
     }
 
     public function jurusan()
     {
         $data = Jurusan::select('id', 'kode', 'nama')->get();
-
-        return $data;
+        
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Jurusan provinsi',
+            'data' => $data
+        ]);  
     }
 
     public function provinsi()
     {
         $data = Provinsi::select('id', 'nama_provinsi')->get();
 
-        return $data;
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Semua provinsi',
+            'data' => $data
+        ]); 
     }
 
     public function kota()
     {
         $data = Kota::get();
-
-        return $data;
+        
+        return response()->json([
+            'status'    => true,
+            'message'   => 'Semua Kota',
+            'data' => $data
+        ]); 
     }
 }
